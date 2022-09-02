d7835f574f88a4f74c493052dec40d3d8ce8efc4
blozds <johaba.jb@gmail.com>
2022-09-02 10:46:04 -0500

    controllercategory

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Controllers;
 
+
+use Illuminate\Http\Response;
 use Illuminate\Http\Request;
+use App\Category;
 
 class CategoryController extends Controller
 {
-    //
-    public function pruebas(Request $request){
-        return "accion de category controller";
+    
+    public function index(){
+        $categories = Category::all();
+        return response()->json([
+            'code' => 200,
+            'status' => 'succes',
+            'categories'=>$categories
+        ]);
     }
 }
